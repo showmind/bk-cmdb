132adf2e8f4848cd68d78f43619eb69942b05f57
lizhihua <lizhihua@xfusion.com>
2025-07-03 11:54:24 +0800

    action 11

--- scripts/tongsuo.sh
+++ scripts/tongsuo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ "${DISABLE_CRYPTO}" = true ];then
+if [ "${DISABLE_CRYPTO}" = true ]; then
   echo "crypto is disabled"
   exit 0
 fi
@@ -18,7 +18,7 @@ tar zxvf 8.3.2.tar.gz
 cd Tongsuo-8.3.2/
 
 if [ "$IS_STATIC" == true ]; then
-  yum -y install glibc-static
+  # yum -y install glibc-static
   ./config --prefix=$TONGSUO_PATH -static
 else
   ./config --prefix=$TONGSUO_PATH
